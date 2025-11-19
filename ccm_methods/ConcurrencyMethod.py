9c3d4d21a5daef25c314a2519649df64073d795c
Mayla Yaffa <maylayaffa12@gmail.com>
2025-11-20 00:34:32 +0700

    refactor: foldering

---
+++ ccm_methods/ConcurrencyMethod.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from ccm_model.Response import Response
+
+class ConcurrencyMethod(ABC):
+
+    @abstractmethod
+    def set_transaction_manager(self, transaction_manager) -> None:
+        pass
+    
+    @abstractmethod
+    def log_object(self, obj, transaction_id: int) -> None:
+        pass
+
+    @abstractmethod
+    def validate_object(self, obj, transaction_id: int, action) -> Response:
+        pass
+
+    @abstractmethod
+    def end_transaction(self, transaction_id: int) -> Response:
+        pass
