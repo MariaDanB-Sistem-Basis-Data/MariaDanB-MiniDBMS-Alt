21be1f36bfcf2fe18ac99d8cd95a00e6d88cd7f5
Nayaka Ghana Subrata <katou7.ayaka@gmail.com>
2025-11-27 19:13:59 +0700

    feat: add reset_instance func (for testing only!)

--- frm_helper/Singleton.py
+++ frm_helper/Singleton.py
@@ -15,7 +15,15 @@ def singleton(cls: Type[T]) -> Type[T]:
                     instances[cls] = cls(*args, **kwargs)
         return instances[cls]
 
+    def reset_instance() -> None: # It is very very important! For testing purposes, do not touch ok TT
+        with lock:
+            if cls in instances:
+                if hasattr(instances[cls], 'initialized'):
+                    delattr(instances[cls], 'initialized')
+                del instances[cls]
+
     get_instance.__name__ = cls.__name__
     get_instance.__doc__ = cls.__doc__
+    get_instance.reset_instance = reset_instance
 
     return get_instance
